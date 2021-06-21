d7a9be481c64e0839d111f1f57acbbc44af434d4
David <da70@nyu.edu>
2021-06-21 15:58:21 -0400

    ui/.eslintrc.js: change from "plugin:vue/essential" to "plugin:vue/recommended

--- ui/.eslintrc.js
+++ ui/.eslintrc.js
@@ -4,7 +4,11 @@ module.exports = {
         node : true
     },
     extends       : [
-        'plugin:vue/essential',
+        // https://github.com/vuejs/eslint-plugin-vue/blob/800182c70fa20df858a46f949313f80e03a0f2ac/lib/configs/base.js
+        // https://github.com/vuejs/eslint-plugin-vue/blob/800182c70fa20df858a46f949313f80e03a0f2ac/lib/configs/essential.js
+        // https://github.com/vuejs/eslint-plugin-vue/blob/800182c70fa20df858a46f949313f80e03a0f2ac/lib/configs/strongly-recommended.js
+        // https://github.com/vuejs/eslint-plugin-vue/blob/800182c70fa20df858a46f949313f80e03a0f2ac/lib/configs/recommended.js
+        'plugin:vue/recommended',
         '@vue/standard'
     ],
     parserOptions : {
