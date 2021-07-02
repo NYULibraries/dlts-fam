1e788c8e20f54bf779b85a1bf542af0328261ae5
David <da70@nyu.edu>
2021-07-02 13:27:09 -0400

    ui/ -- CreateNewFindingAid browser tests: new test "EAD files with invalid elements and unpublished material are rejected with the correct error messages"

--- ui/tests/browser/tests/create-new-finding-aid.js
+++ ui/tests/browser/tests/create-new-finding-aid.js
@@ -16,6 +16,8 @@ const MC_100_EAD_FILE_PATH =
     path.join( __dirname, '../fixtures/mc_100.xml' );
 const MC_100_MISSING_EADID_AND_REPOSITORY_CORPNAME_EAD_FILE_PATH =
     path.join( __dirname, '../fixtures/mc_100-missing-eadid-and-repository-corpname.xml' );
+const MC_100_INVALID_EADID_REPOSITORY_ROLE_RELATOR_CODES_UNPUBLISHED_MATERIAL_EAD_FILE_PATH =
+    path.join( __dirname, '../fixtures/mc_100-invalid-eadid-repository-role-relator-codes-unpublished-material.xml ' );
 
 const MISSING_ELEMENTS_ERRORS_RESULTS_TEXT =
     `Uploading EAD file mc_100-missing-eadid-and-repository-corpname.xml...
@@ -26,6 +28,83 @@ ERROR: Required element <eadid> not found.
 
 ERROR: Required element <repository>/<corpname> not found.
 `;
+
+const INVALID_EADID_REPOSITORY_ROLE_RELATOR_CODES_UNPUBLISHED_MATERIAL_RESULTS_TEXT =
+    `Uploading EAD file mc_100-invalid-eadid-repository-role-relator-codes-unpublished-material.xml...
+Upload complete.
+Validating EAD file...
+---------------------------
+Error: Invalid <repository>
+
+<repository> contains unknown repository name "NYU Archives".
+The repository name must match a value from this list:
+
+Akkasah: Center for Photography (NYU Abu Dhabi)
+New York University Archives
+Center for Brooklyn History
+Fales Library and Special Collections
+Villa La Pietra
+New-York Historical Society
+NYU Abu Dhabi, Archives and Special Collections
+Poly Archives at Bern Dibner Library of Science and Technology
+Tamiment Library and Robert F. Wagner Labor Archives
+
+----------------------
+Error: Invalid <eadid>
+
+<eadid> value "mc.100" does not conform to the Finding Aids specification.
+There must be between 2 to 8 character groups joined by underscores.
+The following characters are not allowed in character groups: .
+
+----------------------------
+Error: Private data detected
+
+The EAD file contains unpublished material.  The following EAD elements have attribute audience="internal" and must be removed:
+
+<bioghist>
+<processinfo>
+
+---------------------------------
+Error: Unrecognized relator codes
+
+The EAD file contains elements with role attributes containing unrecognized relator codes:
+
+<controlaccess><corpname>Columbia University</corpname></controlaccess> has role="orz"
+<controlaccess><corpname>The New School</corpname></controlaccess> has role="cpr"
+<controlaccess><famname>Buell Family</famname></controlaccess> has role="cpo"
+<controlaccess><famname>Lanier Family</famname></controlaccess> has role="fdr"
+<controlaccess><persname>John Doe, 1800-1900</persname></controlaccess> has role="clb"
+<controlaccess><persname>Jane Doe, 1800-1900</persname></controlaccess> has role="grt"
+<origination><corpname>Queens College</corpname></origination> has role="cpr"
+<origination><corpname>Hunter College</corpname></origination> has role="orz"
+<origination><famname>Draper family</famname></origination> has role="fro"
+<origination><persname>Daisy, Bert</persname></origination> has role="clb"
+<origination><persname>Orchid, Ella</persname></origination> has role="grt"
+<repository><corpname>NYU Archives</corpname></repository> has role="grt"
+
+-----------------------------------------------------------------
+
+Please make the necessary corrections and re-upload the EAD file.`;
+
+const EADID_CONFLICT_RESULTS_TEXT =
+    `Uploading EAD file mc_100-eadid-conflict.xml...
+Upload complete.
+Validating EAD file...
+------------------------------------------------------------------------------
+Error: <eadid> conflict with a published finding aid in a different repository
+
+A published finding aid with <eadid> "tam_100" already exists in repository "Tamiment Library and Robert F. Wagner Labor Archives":
+
+EAD ID: tam_100
+REPOSITORY: Tamiment Library and Robert F. Wagner Labor Archives
+TIMESTAMP: 1530045309
+
+The uploaded EAD file belongs to repository "New York University Archives".  <eadid> values must be unique across all repositories.
+In order to create an in-process finding aid from this EAD file, you must either delete the existing published finding aid in repository "Tamiment Library and Robert F. Wagner Labor Archives", or change the <eadid> value in this EAD file.
+
+-----------------------------------------------------------------
+
+Please make the necessary corrections and re-upload the EAD file.`;
 
 const INVALID_XML_FILE_ERROR_RESULTS_TEXT =
     `Uploading EAD file invalid-xml.xml...
@@ -114,6 +193,21 @@ suite( 'Create New Finding Aid', function () {
         );
 
         assert.equal( CreateNewFindingAid.results, MISSING_ELEMENTS_ERRORS_RESULTS_TEXT );
+        assert.isFalse( CreateNewFindingAid.submitButton.isEnabled() );
+    } );
+
+    test( 'EAD files with invalid elements and unpublished material are rejected with the correct error messages', function () {
+        CreateNewFindingAid.uploadFile( MC_100_INVALID_EADID_REPOSITORY_ROLE_RELATOR_CODES_UNPUBLISHED_MATERIAL_EAD_FILE_PATH );
+
+        waitUntil(
+            () => CreateNewFindingAid.results.includes( 'Error' ),
+            'Results text does not include the string "Error"',
+            {
+                timeout    : 10000,
+            },
+        );
+
+        assert.equal( CreateNewFindingAid.results, INVALID_EADID_REPOSITORY_ROLE_RELATOR_CODES_UNPUBLISHED_MATERIAL_RESULTS_TEXT );
         assert.isFalse( CreateNewFindingAid.submitButton.isEnabled() );
     } );
 
