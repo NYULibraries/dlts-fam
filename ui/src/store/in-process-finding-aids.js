3a52f3f8f999361eb36b518c54f3847d6976716f
David <da70@nyu.edu>
2021-07-08 11:56:17 -0400

    ui/ -- refresh fake data for in-process and published finding aids

--- ui/src/store/in-process-finding-aids.js
+++ ui/src/store/in-process-finding-aids.js
@@ -1,9 +1,9 @@
 /* eslint quote-props: ["error", "always"] */
 export default {
     'akkasah' : {
-        'ad_mc_007' : {
-            'timestamp' : 1585758136,
-            'title'     : 'Ozge Calafato Collection AD.MC.007',
+        'ad_mc_012' : {
+            'timestamp' : 1590059378,
+            'title'     : 'Engin Ozendes Collection AD.MC.012',
         },
         'ad_mc_041' : {
             'timestamp' : 1588066811,
@@ -15,17 +15,45 @@ export default {
         },
     },
     'archives' : {
-        'iaaa' : {
-            'timestamp' : 1587579914,
-            'title'     : 'Records of the Institute of African American Affairs RG.9.8',
+        'apaimport2' : {
+            'timestamp' : 1624469067,
+            'title'     : 'Records of the Asian/Pacific/American Institute RG.21.35',
         },
-        'rg_12_10' : {
-            'timestamp' : 1578933820,
-            'title'     : 'Black Student Service Center Records RG.12.10',
+        'cullen' : {
+            'timestamp' : 1608585918,
+            'title'     : 'New York University Archives Collection on Countee Cullen 277',
         },
-        'washsquarephoto' : {
-            'timestamp' : 1584539268,
-            'title'     : 'New York University Archives Photograph Collection PHOTO.00001',
+        'eagle' : {
+            'timestamp' : 1621438814,
+            'title'     : 'Photographs from the Brooklyn Daily Eagle BCMS.0002',
+        },
+        'mc_139' : {
+            'timestamp' : 1625682278,
+            'title'     : 'Alice Keliher Papers MC.139',
+        },
+        'mc_3' : {
+            'timestamp' : 1625680860,
+            'title'     : 'Marion Eugenie Bauer Papers MC.3',
+        },
+        'mc_354' : {
+            'timestamp' : 1617915018,
+            'title'     : 'New York University Archives Collection on Virtual Commencement Celebrations Webpages MC.354',
+        },
+        'mc_41' : {
+            'timestamp' : 1625681265,
+            'title'     : 'Francis Hovey Stoddard Papers MC.41',
+        },
+        'rg_2_0_2' : {
+            'timestamp' : 1625682516,
+            'title'     : 'Rev. George Alexander Papers RG.2.0.2',
+        },
+        'rg_35' : {
+            'timestamp' : 1625682696,
+            'title'     : 'Records of the Office of the Dean of the Tisch School of the Arts RG.35',
+        },
+        'rg_39_178' : {
+            'timestamp' : 1610038656,
+            'title'     : 'New York University Archives Collection of the Washington Square News RG.39.178',
         },
         'fadesign_116_changed' : {
             'timestamp' : 936882549,
@@ -45,83 +73,59 @@ export default {
         },
     },
     'bhs' : {
-        '1974.002' : {
-            'timestamp' : 1530124091,
-            'title'     : 'Colonial manuscripts collection 1974.002',
-        },
         '1979_008' : {
             'timestamp' : 1539378584,
             'title'     : 'Ditmars family correspondence 1782',
         },
-        '2012-019' : {
-            'timestamp' : 1563388802,
-            'title'     : 'Raymond Love films of the Verrazano-Narrows Bridge construction 2012.019',
+        '2018_006' : {
+            'timestamp' : 1597096117,
+            'title'     : 'Muslims in Brooklyn oral histories 2018.006',
         },
-        '2016.027_voices_crown_heights' : {
-            'timestamp' : 1513710863,
-            'title'     : 'Voices of Crown Heights oral histories 2016.027',
+        'arms_2009_011_lapc' : {
+            'timestamp' : 1609272642,
+            'title'     : 'Lafayette Avenue Presbyterian Church records 2009.011',
         },
-        '2016_026' : {
-            'timestamp' : 1540326912,
-            'title'     : 'Brooklyn For Peace records 3552',
+        'bcms_0002' : {
+            'timestamp' : 1616601271,
+            'title'     : 'Brooklyn Daily Eagle photographs BCMS.0002',
         },
-        'ARC.315' : {
-            'timestamp' : 1540999309,
-            'title'     : 'Brooklyn Historical Society vertical files ARC.315',
+        'bcms_0007' : {
+            'timestamp' : 1621533293,
+            'title'     : 'Brooklyn Ephemera Collection BCMS.0007',
         },
-        'arms_1993_001_aids-brooklyn' : {
-            'timestamp' : 1553109437,
-            'title'     : 'AIDS/Brooklyn Oral History Project collection 1993.001',
+        'bcms_0031' : {
+            'timestamp' : 1614790204,
+            'title'     : 'Brooklyn Yearbook Collection BCMS.0031',
+        },
+        'bcms_0037' : {
+            'timestamp' : 1619818469,
+            'title'     : 'The Froebel Society Records: Brooklyn Chapter BCMS.0037',
+        },
+        'bcms_0041' : {
+            'timestamp' : 1619819620,
+            'title'     : 'Brooklyn Playbills and Programs Collection BCMS.0041',
         },
     },
     'fales' : {
-        'air' : {
-            'timestamp' : 1584041767,
-            'title'     : 'A.I.R. Gallery Archives MSS.184',
+        'lapietra' : {
+            'timestamp' : 1614617635,
+            'title'     : 'The Acton Photograph Archive MSS.LaPietra.001',
         },
-        'kolm' : {
-            'timestamp' : 1579123640,
-            'title'     : 'Ron Kolm Papers MSS.079',
-        },
-        'mss_421' : {
-            'timestamp' : 1584538661,
-            'title'     : 'Richard Berkowitz Papers MSS.421',
-        },
-        'mss_442' : {
-            'timestamp' : 1580398466,
-            'title'     : 'Chris Kraus Papers MSS.442',
-        },
-        'mss_460' : {
-            'timestamp' : 1584539139,
-            'title'     : 'Adele Fournet Papers on the Bit Rosie Web Series MSS.460',
-        },
-        'mss_480' : {
-            'timestamp' : 1582145614,
-            'title'     : 'Julie Kathryn Franco I Am Snow Angel Papers MSS.480',
+        'mss_221' : {
+            'timestamp' : 1624307551,
+            'title'     : 'Sylvère Lotringer Papers and Semiotext(e) Archive MSS.221',
         },
     },
     'lapietra' : {
     },
     'nyhs' : {
-        'Guide to the Cased Image File' : {
-            'timestamp' : 1466094511,
-            'title'     : 'Guide to the Cased Image File (Test) PR5507',
-        },
-        'architecture (test)' : {
-            'timestamp' : 1467913880,
-            'title'     : 'Architecture PR 5522',
-        },
-        'csv test 3' : {
-            'timestamp' : 1445459253,
-            'title'     : 'Preliminary Inventory to the test3',
-        },
-        'envelope test' : {
-            'timestamp' : 1468939355,
-            'title'     : 'Guide to the Patriotic Envelope Collection (test) PR 5525',
-        },
         'fineart' : {
             'timestamp' : 1458157286,
             'title'     : 'Fine Art Prints of New York City PR 229',
+        },
+        'gilbert' : {
+            'timestamp' : 1601929331,
+            'title'     : 'Cass Gilbert papers PR 21',
         },
         'gilbertdrawings' : {
             'timestamp' : 1568641530,
@@ -131,33 +135,33 @@ export default {
             'timestamp' : 1408733161,
             'title'     : 'New York Inventories, 1680-1848 (bulk 1726-1844) MS 450.2',
         },
-        'kane-greenbergtest' : {
-            'timestamp' : 1466705015,
-            'title'     : 'Kane-Greenberg Collection of Progressive Proof Prints PR 242',
-        },
         'mckim' : {
-            'timestamp' : 1588266697,
-            'title'     : 'McKim, Mead and White architectural records PR 42',
+            'timestamp' : 1597255033,
+            'title'     : 'McKim, Mead and White architectural records 334',
         },
-        'ms2958.9788_varrecchia' : {
-            'timestamp' : 1519160553,
-            'title'     : 'Alfonso Varrechia passport ms2958_9788',
-        },
-        'ms2999_rentschler' : {
-            'timestamp' : 1588279809,
-            'title'     : 'Mary Coolidge Rentschler Collection MS 2999',
-        },
-        'ms3070_newman' : {
-            'timestamp' : 1546032776,
-            'title'     : 'Lawrence Newman papers MS 3070',
+        'ms201_erasmus_hall' : {
+            'timestamp' : 1616438694,
+            'title'     : 'Erasmus Hall records MS 201',
         },
         'ms3093_maritime_association' : {
-            'timestamp' : 1583422016,
+            'timestamp' : 1597263746,
             'title'     : 'Maritime Association of the Port of New York records MS 3093',
         },
-        'ms3147_billie_jean_king' : {
-            'timestamp' : 1582220470,
-            'title'     : 'Billie Jean King Collection MS 3147',
+        'ms3199_womens_sports' : {
+            'timestamp' : 1623962230,
+            'title'     : 'The Women\'s Sports Foundation records MS 3199',
+        },
+        'ms3202_bernfeld_puppet' : {
+            'timestamp' : 1625756368,
+            'title'     : 'Beatrice Bernfeld puppet play scripts and stories MS 3202',
+        },
+        'ms324_irvin' : {
+            'timestamp' : 1603719766,
+            'title'     : 'Richard Irvin &amp; Co. records MS 324',
+        },
+        'ms443_9_stmarks_church_bowery' : {
+            'timestamp' : 1625078593,
+            'title'     : 'St. Mark\'s Church in-the-Bowery records MS 443.9',
         },
         'ms531_rutherfurd_collection' : {
             'timestamp' : 1586957124,
@@ -167,55 +171,47 @@ export default {
             'timestamp' : 1546204750,
             'title'     : 'New-York Historical Society Board of Trustees records NYHS-RG 18',
         },
-        'nyhsrg20' : {
-            'timestamp' : 1546805438,
-            'title'     : 'New-York Historical Society Museum Department records NYHS-RG 20',
-        },
         'nyhsrg22' : {
             'timestamp' : 1546206224,
             'title'     : 'New-York Historical Society Executive Office records NYHS-RG 22',
         },
-        'portrait' : {
-            'timestamp' : 1587752475,
-            'title'     : 'New-York Historical Society portrait collection PR 52',
-        },
         'pr010_caricatures' : {
-            'timestamp' : 1583854694,
+            'timestamp' : 1599165741,
             'title'     : 'New-York Historical Society collection of caricatures and cartoons PR 10',
         },
-        'pr325_kalinsky' : {
-            'timestamp' : 1547735729,
-            'title'     : 'George Kalinsky photographs PR 325',
+        'pr053_post' : {
+            'timestamp' : 1625249077,
+            'title'     : 'George B. Post architectural records PR 053',
         },
         'pr412_william_oberhardt' : {
             'timestamp' : 1582569488,
             'title'     : 'William Oberhardt drawings and papers PR 412',
         },
-        'stereograph' : {
-            'timestamp' : 1588089403,
-            'title'     : 'Stereograph File PR 65',
+        'pr425_william_butler' : {
+            'timestamp' : 1625579339,
+            'title'     : 'William G. Butler photography collection PR 425',
         },
         'timeincnewtest' : {
             'timestamp' : 1512500939,
             'title'     : 'Time Inc. Records TEST MS 3009-RG NewTest',
         },
-        'timeincrg12' : {
-            'timestamp' : 1582557524,
-            'title'     : 'Time Inc. Architectural Forum and House &amp; Home Editorial Records: Managing Editor Edgar P. Smith Files MS 3009-RG 12',
+        'timeincrg17' : {
+            'timestamp' : 1625073350,
+            'title'     : 'Time Inc. Human Resources and Personnel Records MS 3009-RG 17',
         },
         'timeincrg24new' : {
             'timestamp' : 1558033398,
             'title'     : 'NEW Time Inc. Time Publishing and Business Records MS 3009-RG 24new',
-        },
-        'watts-jones' : {
-            'timestamp' : 1397065763,
-            'title'     : 'Watts-Jones Family Papers MS 681',
         },
     },
     'nyuad' : {
         'ad_mc_001' : {
             'timestamp' : 1522149562,
             'title'     : 'Vardan Hovhannisyan video collection AD.MC.001',
+        },
+        'ad_mc_004' : {
+            'timestamp' : 1623582075,
+            'title'     : 'NYU Abu Dhabi Capstone Archives 12',
         },
         'ad_mc_012' : {
             'timestamp' : 1524763494,
@@ -225,17 +221,25 @@ export default {
             'timestamp' : 1585848150,
             'title'     : 'Scenes of Egypt During WWI AD.MC.015',
         },
+        'ad_rg_24_1' : {
+            'timestamp' : 1625397788,
+            'title'     : 'NYU Abu Dhabi Institute: Public Program Materials AD.RG.024.1',
+        },
         'ad_rg_2_1' : {
             'timestamp' : 1580398580,
             'title'     : 'NYU Abu Dhabi Publications and Ephemera AD.RG.2.1',
+        },
+        'ad_rg_9_001' : {
+            'timestamp' : 1616650130,
+            'title'     : 'Records of NYU Abu Dhabi Student Government AD.RG.9.001',
         },
         'capstone' : {
             'timestamp' : 1509102469,
             'title'     : 'NYU Abu Dhabi Capstone Archives AD MC 004',
         },
         'oralhist' : {
-            'timestamp' : 1484324270,
-            'title'     : 'NYUAD Oral History Project AD MC 023',
+            'timestamp' : 1610283129,
+            'title'     : 'NYU Abu Dhabi Library Oral History Project AD.MC.023',
         },
         'timeincrg2' : {
             'timestamp' : 1492094085,
@@ -259,43 +263,39 @@ export default {
             'timestamp' : 1427388331,
             'title'     : 'Guide to the Poly Archives Video Collection, 1975-2007 RG.003',
         },
-        'test tech report' : {
-            'timestamp' : 1430841723,
-            'title'     : 'TEST MRI technical report TEST RG.031',
-        },
     },
     'tamwag' : {
-        'aia_074' : {
-            'timestamp' : 1582824651,
-            'title'     : 'Charles Laverty Papers AIA.074',
+        'faker_fake' : {
+            'timestamp' : 1619101755,
+            'title'     : 'New York City Immigrant Labor History Project Oral History Collection OH.014',
         },
-        'aia_080' : {
-            'timestamp' : 1584537892,
-            'title'     : 'Irish Repertory Theatre Records AIA.080',
+        'tam_007_005' : {
+            'timestamp' : 1625680496,
+            'title'     : 'Rand School of Social Science: Publications TAM.007.005',
         },
-        'tam_105' : {
-            'timestamp' : 1582218569,
-            'title'     : 'Democratic Socialists of America Records TAM.105',
+        'tam_107' : {
+            'timestamp' : 1625682271,
+            'title'     : 'Tamiment Playhouse Records TAM.107',
         },
-        'tam_439' : {
-            'timestamp' : 1584537671,
-            'title'     : 'Camp Kinderland Records TAM.439',
+        'tam_273' : {
+            'timestamp' : 1625680040,
+            'title'     : 'Irving Adler Papers TAM.273',
         },
-        'tam_661' : {
-            'timestamp' : 1582224961,
-            'title'     : 'Penina Coopersmith Collection on Student Radicalism TAM.661',
+        'tam_403' : {
+            'timestamp' : 1625688509,
+            'title'     : 'Moss Roberts Vietnam War Research Files TAM.403',
         },
-        'tam_687' : {
-            'timestamp' : 1584538249,
-            'title'     : 'Robert Gruenwald Papers TAM.687',
+        'tam_520' : {
+            'timestamp' : 1625682094,
+            'title'     : 'Tamiment Library Collection of Noam Chomsky Correspondence and Other Materials Relating to the Faurisson Case TAM.520',
         },
-        'tam_691' : {
-            'timestamp' : 1584538441,
-            'title'     : 'David Havens Photographs on the Cuban Revolution TAM.691',
+        'tam_694' : {
+            'timestamp' : 1619097890,
+            'title'     : 'Rabbi Arthur Schneier Papers TAM.694',
         },
-        'wag_249' : {
-            'timestamp' : 1583782422,
-            'title'     : 'Textile Workers Union of America Scrapbooks WAG.249',
+        'wag_273' : {
+            'timestamp' : 1616701323,
+            'title'     : 'Morris Rosenzweig Papers WAG.273',
         },
     },
 };
