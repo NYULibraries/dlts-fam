47d952d0ac08e18930033b3ed153fbddfe28b9fc
David <da70@nyu.edu>
2021-07-14 21:51:58 -0400

    ui/ -- ManageInProcessFindingAidsPage: add click detail row button methods

--- ui/tests/browser/pageobjects/manage-in-process-finding-aids.page.js
+++ ui/tests/browser/pageobjects/manage-in-process-finding-aids.page.js
@@ -6,6 +6,22 @@ class ManageInProcessFindingAids extends ManageFindingAidsPage {
     get path() {
         return '#/in-process';
     }
+
+    clickDeleteInProcessFindingAidButtonForRow( id ) {
+        this.detailsRow( id ).$( '#delete-in-process-finding-aid-button' ).click();
+    }
+
+    clickPublishFindingAidButtonForRow( id ) {
+        this.detailsRow( id ).$( '#publish-finding-aid-button' ).click();
+    }
+
+    clickViewFindingAidPreviewButtonForRow( id ) {
+        this.detailsRow( id ).$( '#view-finding-aid-preview-button' ).click();
+    }
+
+    clickViewEadFilePreviewButtonForRow( id ) {
+        this.detailsRow( id ).$( '#view-ead-file-preview-button' ).click();
+    }
 }
 
 export default new ManageInProcessFindingAids();
