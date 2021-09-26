67507414ace7848873cd5441b1e2b116141950d2
David <da70@nyu.edu>
2021-09-25 22:50:52 -0400

    ui/ -- manage in-process tests: replace the ad-hoc test stuff with golden files test loop

--- ui/tests/browser/tests/manage-in-process-finding-aids.js
+++ ui/tests/browser/tests/manage-in-process-finding-aids.js
@@ -1,5 +1,7 @@
 /* global setup:false suite:false test:false */
 
+import fs from 'fs';
+
 import { assert } from 'chai';
 
 import ManageInProcessFindingAids from '../pageobjects/manage-in-process-finding-aids.page';
@@ -18,6 +20,8 @@ import {
     SUITE_NAME,
 } from '../util';
 
+const goldenFiles = getGoldenFiles( SUITE_NAME.manageInProcessFindingAids );
+
 suite( 'Manage In-process Finding Aids', function () {
     suiteSetup( function () {
         clearActualFilesDirectory( SUITE_NAME.manageInProcessFindingAids );
@@ -33,41 +37,13 @@ suite( 'Manage In-process Finding Aids', function () {
         Logout.logout();
     } );
 
-    test( 'test', function () {
-        const resultsIds = [];
-        const snapshots = [];
+    goldenFiles.forEach( ( goldenFile ) => {
+        const golden = require( goldenFile );
 
-        snapshots.push( ManageInProcessFindingAids.resultsSnapshot() );
-
-        ManageInProcessFindingAids.setResultsPerPage( 50 );
-        snapshots.push( ManageInProcessFindingAids.resultsSnapshot() );
-        resultsIds.push( ManageInProcessFindingAids.getResultsIdForCurrentOptions() );
-
-        ManageInProcessFindingAids.clickPageNavigationLink( 2 );
-        snapshots.push( ManageInProcessFindingAids.resultsSnapshot() );
-        resultsIds.push( ManageInProcessFindingAids.getResultsIdForCurrentOptions() );
-
-        ManageInProcessFindingAids.filterById( 'a b c ! % *' );
-        snapshots.push( ManageInProcessFindingAids.resultsSnapshot() );
-        resultsIds.push( ManageInProcessFindingAids.getResultsIdForCurrentOptions() );
-
-        ManageInProcessFindingAids.clickSortByTimestamp();
-        snapshots.push( ManageInProcessFindingAids.resultsSnapshot() );
-        resultsIds.push( ManageInProcessFindingAids.getResultsIdForCurrentOptions() );
-
-        ManageInProcessFindingAids.clickSortByTimestamp();
-        snapshots.push( ManageInProcessFindingAids.resultsSnapshot() );
-        resultsIds.push( ManageInProcessFindingAids.getResultsIdForCurrentOptions() );
-
-        ManageInProcessFindingAids.filterByRepository(
-            'Akkasah: Center for Photography (NYU Abu Dhabi)',
-        );
-        snapshots.push( ManageInProcessFindingAids.resultsSnapshot() );
-        resultsIds.push( ManageInProcessFindingAids.getResultsIdForCurrentOptions() );
-
-        assert( true );
+        testFilterResults( golden );
     } );
 } );
+
 function getTestTitleFromGoldenFile( golden ) {
     return 'Filter options { ' +
            ( golden.ui.idFilter ? ` id filter=${ golden.ui.idFilter };` : ' no id filter;' ) +
