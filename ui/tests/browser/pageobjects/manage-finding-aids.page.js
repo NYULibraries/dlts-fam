9eff81f4f5805a14fb428835bd80c334e090db1b
David <da70@nyu.edu>
2021-09-27 17:07:44 -0400

    ui/ -- ManageFindingAidsPage resultsSnapshot: add sort

--- ui/tests/browser/pageobjects/manage-finding-aids.page.js
+++ ui/tests/browser/pageobjects/manage-finding-aids.page.js
@@ -256,6 +256,7 @@ export default class ManageFindingAidsPage extends Page {
                 repositoryFilter : this.repositoryFilterValue || 'All',
                 resultsPerPage   : this.resultsPerPage,
                 resultStats      : this.resultStats,
+                sort             : this.sort,
             },
             results : resultRows,
         };
